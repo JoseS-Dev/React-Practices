0fc6a505552e307fd48514eaaf80d0f71da7d6e4
JoSantana0005 <joseasantana05@gmail.com>
2025-05-04 16:58:45 -0400

    UseState

--- Class/Props/src/App.jsx
+++ Class/Props/src/App.jsx
@@ -1,13 +1,37 @@
 
 import TwitterCard from "./Components/TwitterCard";
 import './Css/App.css';
-
+const users = [
+  {
+    username: "midudev",
+    name: "Miguel Angel",
+    isFollowing: true,
+  },
+  {
+    username: "rafaelgonzalez",
+    name: "Rafael Gonzalez",
+    isFollowing: false,
+  },
+  {
+    username: "pablomontesr",
+    name: "Pablo Montes R",
+    isFollowing: true,
+  },
+];
 export default function App() {
   const FormatUsername = (username) => `@${username}`;
   return (
     <div className="Js-main">
-      <TwitterCard username={"midudev"} name={"Miguel Angel Durán"} Following={true} FormatUsername={FormatUsername}/>
-      <TwitterCard username={"elonmusk"} name={"Elon Musk"} Following={false} FormatUsername={FormatUsername}/>
+      {users.map(({ username, name, isFollowing }) => (
+        <TwitterCard
+          key={username}
+          username={username}
+          Following={isFollowing}
+          FormatUsername={FormatUsername}
+        >
+          {name}
+        </TwitterCard>
+      ))}
     </div>
   )
 }
--- Class/Props/src/Components/TwitterCard.jsx
+++ Class/Props/src/Components/TwitterCard.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 import '../Css/TwitterCard.css';
 
-export default function TwitterCard({username, name, Following, FormatUsername}) {
-    // Creo un componente de botón que recibe un texto como prop y lo muestra en el botón.
+export default function TwitterCard({children,username, Following, FormatUsername}) {
     const [isFollowing, setFollowing] = useState(Following);
     const Text = isFollowing ? "Siguiendo" : "Seguir";
+    
     const className = isFollowing ? "Js-ButtonCard Js-ButtonCard--isFollowing" : "Js-ButtonCard Js-ButtonCard--isFollow";
 
     const handleClick = () => {
@@ -17,12 +17,15 @@ export default function TwitterCard({username, name, Following, FormatUsername})
                 <div className='Js-ImageCard'>
                     <img src={`https://unavatar.io/${username}`}/>
                     <div className='Js-TextCard'>
-                        <h4>{name}</h4>
+                        <h4>{children}</h4>
                         <p>{FormatUsername(username)}</p>
                     </div>
                 </div>
                 <div className='Js-ButtonContainer'>
-                <button className={className} onClick={() => handleClick()}>{Text}</button>
+                <button className={className} onClick={() => handleClick()}>
+                    <span className={className}>{Text}</span>
+                    <span className="Js-ButtonCard--notFollowing">Dejar de Seguir</span>
+                </button>
                 </div>
             </article>
          </div>
